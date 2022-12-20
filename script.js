1163b5ad27fed15027df59b5422d913044e2150b
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2022-12-20 12:35:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function openNav() {
-    document.getElementById("slidemenu").style.width = "100%";
-    console.log("hello");
+  document.getElementById('slidemenu').style.width = '100%';
+  console.log('hello');
 
 }
 openNav();
 
 
 function closeNav() {
-    document.getElementById("slidemenu").style.width = "0";
-    console.log();
+  document.getElementById('slidemenu').style.width = '0';
+  console.log('goodbye');
 
 }
 closeNav();
