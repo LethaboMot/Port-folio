44256f5555ea43b49ba7517f0d4deb103f84e22e
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2022-12-24 03:04:55 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -47,45 +47,45 @@ btn.onclick = function() {
 };
 
 span.onclick = function() {
-    modal.style.display = "none';
+    modal.style.display = 'none';
 };
 
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = 'none';
     };
 };
 
 // second card
-var modal2 = document.getElementById("myModal2");
-var btn = document.getElementById("myBtn2");
-var span = document.getElementsByClassName("close2")[0];
+var modal2 = document.getElementById('myModal2');
+var btn = document.getElementById('myBtn2');
+var span = document.getElementsByClassName('close2')[0];
 
 btn.onclick = function() {
-    modal2.style.display = "block";
+    modal2.style.display = 'block';
 };
 
 span.onclick = function() {
-    modal2.style.display = "none";
+    modal2.style.display = 'none';
 };
 
 window.onclick = function(event) {
         if (event.target == modal2) {
-            modal2.style.display = "none";
+            modal2.style.display = 'none';
         };
     };
     //  third card
-var modal3 = document.getElementById("myModal3");
-var btn = document.getElementById("myBtn3");
-var span = document.getElementsByClassName("close3")[0];
+var modal3 = document.getElementById('myModal3');
+var btn = document.getElementById('myBtn3');
+var span = document.getElementsByClassName('close3')[0];
 
 btn.onclick = function() {
-    modal3.style.display = "block";
+    modal3.style.display = 'block';
 }
 
 
 span.onclick = function() {
-    modal3.style.display = 'none";
+    modal3.style.display = 'none';
 }
 
 window.onclick = function(event) {
