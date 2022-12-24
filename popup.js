16451988fce47c9b78fca031b3f24162a5065347
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2022-12-24 11:24:54 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,41 +1,41 @@
-const projectbtn = document.getElementById('myBtn1');
-const bluebutton = document.getElementById( '.bluebutton' );
-const projects = [{
-  card: 1,
-  name: 'Tonic',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since th',
-  image: 'img/Nature',
-  technologies: ['html', 'JavaScript', 'css'],
-  source: 'https://github.com/LethaboMot',
-  link: 'https://lethabomot.github.io/Port-folio/',
-},
-{
-  card: 2,
-  name: 'Multi-Post Stories ',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since th',
-  image: 'img/Nature',
-  technologies: ['html', 'JavaScript', 'css'],
-  link: 'https://lethabomot.github.io/Port-folio/',
-},
-{
-  card: 3,
-  name: 'Facebook',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the',
-  image: 'img/Nature',
-  technologies: ['html', 'JavaScript', 'css'],
-  source: 'https://github.com/LethaboMot',
-  link: 'https://lethabomot.github.io/Port-folio/',
-},
-{
-  card: 4,
-  name: 'Uber Navigation Stories',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since th',
-  image: 'img/Nature',
-  technologies: ['html', 'JavaScript', 'css'],
-  source: 'https://github.com/LethaboMot',
-  link: 'https://lethabomot.github.io/Port-folio/',
-};
-];
+// const projectbtn = document.getElementById('myBtn1');
+// const bluebutton = document.getElementById( '.bluebutton' );
+// const projects = [{
+//   card: 1,
+//   name: 'Tonic',
+//   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since th',
+//   image: 'img/Nature',
+//   technologies: ['html', 'JavaScript', 'css'],
+//   source: 'https://github.com/LethaboMot',
+//   link: 'https://lethabomot.github.io/Port-folio/',
+// },
+// {
+//   card: 2,
+//   name: 'Multi-Post Stories ',
+//   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since th',
+//   image: 'img/Nature',
+//   technologies: ['html', 'JavaScript', 'css'],
+//   link: 'https://lethabomot.github.io/Port-folio/',
+// },
+// {
+//   card: 3,
+//   name: 'Facebook',
+//   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the',
+//   image: 'img/Nature',
+//   technologies: ['html', 'JavaScript', 'css'],
+//   source: 'https://github.com/LethaboMot',
+//   link: 'https://lethabomot.github.io/Port-folio/',
+// },
+// {
+//   card: 4,
+//   name: 'Uber Navigation Stories',
+//   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since th',
+//   image: 'img/Nature',
+//   technologies: ['html', 'JavaScript', 'css'],
+//   source: 'https://github.com/LethaboMot',
+//   link: 'https://lethabomot.github.io/Port-folio/',
+// };
+// ];
 
 var modal = document.getElementById('myModal');
 var btn = document.getElementById('myBtn');
