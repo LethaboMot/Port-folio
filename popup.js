7e3abb81fc9e0a7eaa62a99545a188ffedf82df4
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2022-12-24 11:43:00 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,12 +2,12 @@ const modal = document.getElementById('myModal');
 const btn = document.getElementById('myBtn');
 const span = document.getElementsByClassName('close')[0];
 
-btn.onclick = function() {
+btn.onclick = function () {
     modal.style.display = 'block';
 }
 
-span.onclick = function() {
-    modal.style.display = 'none';
+span.onclick = function () {
+  modal.style.display = 'none';
 }
 
 window.onclick = function(event) {
@@ -21,52 +21,52 @@ const modal2 = document.getElementById('myModal2');
 const btn2 = document.getElementById('myBtn2');
 const span2 = document.getElementsByClassName('close2')[0];
 
-btn2.onclick = function() {
-    modal2.style.display = 'block';
+btn2.onclick = function () {
+  modal2.style.display = 'block';
 }
 
-span2.onclick = function() {
+span2.onclick = function () {
     modal2.style.display = 'none';
 }
 
 window.onclick = function(event) {
-        if (event.target == modal2) {
-            modal2.style.display = 'none';
-        }
-    }
+  if (event.target == modal2) {
+    modal2.style.display = 'none';
+}
+}
     //  third card
 const modal3 = document.getElementById('myModal3');
 const btn3 = document.getElementById('myBtn3');
 const span3 = document.getElementsByClassName('close3')[0];
 
-btn.onclick = function() {
-    modal3.style.display = 'block';
+btn.onclick = function () {
+  modal3.style.display = 'block';
 }
 
-span.onclick = function() {
-    modal3.style.display = 'none';
+span.onclick = function () {
+  modal3.style.display = 'none';
 }
 
 window.onclick = function(event) {
-        if (event.target == modal3) {
-            modal3.style.display = 'none';
- }
+  if (event.target == modal3) {
+    modal3.style.display = 'none';
+}
 }
     //  fourth card
 const modal4 = document.getElementById('myModal4');
 const btn4 = document.getElementById('myBtn4');
 const span4 = document.getElementsByClassName('close4')[0];
 
-btn.onclick = function() {
-    modal4.style.display = 'block';
+btn.onclick = function () {
+  modal4.style.display = 'block';
 }
 
-span.onclick = function() {
-    modal4.style.display = 'none';
+span.onclick = function () {
+  modal4.style.display = 'none';
 }
 
 window.onclick = function(event) {
-    if (event.target == modal4) {
-        modal4.style.display = 'none';
- }
+  if (event.target == modal4) {
+    modal4.style.display = 'none';
+}
 }
