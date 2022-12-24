0d42dd4f1c988c193591dc91f8648940305be9c5
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2022-12-24 04:05:23 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -8,7 +8,6 @@ const projects = [{
   technologies: ['html', 'JavaScript', 'css'],
   source: 'https://github.com/LethaboMot',
   link: 'https://lethabomot.github.io/Port-folio/',
-  projects = 'card: 1';
 },
 {
   card: 2,
