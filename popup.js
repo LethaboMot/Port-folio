09ff330455182a6a83e4ef25107f204e0c9a7d34
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-01-03 21:21:00 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,27 +1,27 @@
- const popup = document.getElementById('myPopup');
- const btn = document.getElementById('myBtn');
- const span = document.getElementsByClassName('close')[0];
+const popup = document.getElementById('myPopup');
+const btn = document.getElementById('myBtn');
+const span = document.getElementsByClassName('close')[0];
 
- const popup1 = document.getElementById('myPopup1');
- const btn1 = document.getElementById('myBtn1');
- const span1 = document.getElementsByClassName('close1')[0];
+const popup1 = document.getElementById('myPopup1');
+const btn1 = document.getElementById('myBtn1');
+const span1 = document.getElementsByClassName('close1')[0];
 
- const popup2 = document.getElementById('myPopup2');
- const btn2 = document.getElementById('myBtn2');
- const span2 = document.getElementsByClassName('close2')[0];
+const popup2 = document.getElementById('myPopup2');
+const btn2 = document.getElementById('myBtn2');
+const span2 = document.getElementsByClassName('close2')[0];
 
- const popup3 = document.getElementById('myPopup3');
- const btn3 = document.getElementById('myBtn3');
- const span3 = document.getElementsByClassName('close3')[0];
+const popup3 = document.getElementById('myPopup3');
+const btn3 = document.getElementById('myBtn3');
+const span3 = document.getElementsByClassName('close3')[0];
 
- const projects = [{
-   card: 1,
-   name: 'Tonic',
-   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since th',
-   image: './img/images/Nature',
-   technologies: ['html', 'JavaScript', 'css'],
-   source: 'https://github.com/LethaboMot',
-   link: 'https://lethabomot.github.io/Port-folio/',
+const projects = [{
+  card: 1,
+  name: 'Tonic',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since th',
+  image: './img/images/Nature',
+  technologies: ['html', 'JavaScript', 'css'],
+  source: 'https://github.com/LethaboMot',
+  link: 'https://lethabomot.github.io/Port-folio/',
 },
 {
   card: 2,
@@ -49,24 +49,24 @@
   technologies: ['html', 'JavaScript', 'css'],
   source: 'https://github.com/LethaboMot',
   link: 'https://lethabomot.github.io/Port-folio/',
-}
+};
 ];
 
- btn.onclick = function () {
+btn.onclick = function () {
   popup.style.display = 'block';
  };
 
- span.onclick = function () {
+span.onclick = function () {
   popup.style.display = 'none';
- };
+};
 
- window.onclick = function (event) {
+window.onclick = function (event) {
   if (event.target === popup) {
     popup.style.display = 'none';
-     }
- };
+  }
+};
 
- // second card
+// second card
 
 btn1.onclick = function () {
   popup1.style.display = 'block';
@@ -100,15 +100,15 @@ window.onclick = function (event) {
  //  fourth card
 
 btn3.onclick = function () {
-   popup3.style.display = 'block';
+  popup3.style.display = 'block';
 };
 
 span3.onclick = function () {
   popup3.style.display = 'none';
- };
+};
 
- window.onclick = function (event) {
+window.onclick = function (event) {
   if (event.target === popup3) {
-         popup3.style.display = 'none';
+    popup3.style.display = 'none';
   }
  };
